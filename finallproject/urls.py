a3876b8be63f888ab4000888bd50f8b542b6effa
MikeAyrton <82711970+MikeAyrton@users.noreply.github.com>
2022-04-10 11:43:18 +0500

    111115o

--- finallproject/urls.py
+++ finallproject/urls.py
@@ -14,18 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
-
-app_name="projectapp"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projectapp.urls')),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
